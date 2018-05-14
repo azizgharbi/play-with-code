7dbfea5944b1319465335e8ebd209894f291c52a
aziz gharbi <agharbi@inovva.com>
2018-05-14 12:53:01 -0400

    test prototype

---
+++ composition.js
@@ -0,0 +1,34 @@
+/**
+ * compositions
+ */
+
+/* https://medium.com/front-end-hacking/classless-javascript-composition-over-inheritance-6b27c35893b1 */
+
+ //In real life this method/function will be bigger
+//So taking this function out of createPerson will make sense then
+function getInfo(firstName, lastName){
+    return "Hi i am " + firstName + "" + lastName;
+  }
+   
+  function createPerson(fn, ln){
+    return {
+      getInfo: function(){ return getInfo(fn, ln)} 
+    };
+  }
+   
+  //In real life this method/function will be bigger
+  //So taking this function out of createPerson will make sense then
+  function getId(empid){
+    return "Hi my employee id is " + empid;
+  }
+   
+  //createEmployee makes use of composition. Its’ simple. Any function //which makes use of other functions to compose functionality is //said to be making use of composition. We all use it many times.
+  function createEmployee(fn, ln, empid){
+    return {
+      getId : function(){ return getId(empid);},
+      getInfo: function(){ return getInfo(fn, ln);} 
+    };
+  }
+  var e2 = createEmployee("john", "doe", 123);
+  console.log( e2.getInfo() );// Hi i am john doe
+  console.log( e2.getId() ); // Hi my employee id is 123
---
+++ prototype.js
@@ -0,0 +1,44 @@
+const person = {
+
+    init : function(name){
+         this.name = name ;
+    },
+    myName : function(){
+        console.log("My name is: " + this.name);
+    }
+}
+
+const aziz = Object.create(person);
+const dave = Object.create(person);
+const aron = Object.create(person);
+
+
+aziz.init("aziz");
+
+dave.init("David");
+
+ /**
+  * uncomment this line to test the result
+  */
+
+//aron.init("Aron");
+
+
+
+
+dave.myName = function(){
+    console.log("OHHH REALLY OMG " + this.name);
+}
+
+aziz.myName();
+
+dave.myName();
+
+person.name = "this is not a name";
+
+
+aron.myName();
+
+console.log(person.isPrototypeOf(aziz));
+
+console.log(person.isPrototypeOf(aron));
