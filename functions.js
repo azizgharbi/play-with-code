3ff797364752b45ff38ffbfe75b3dbe8b07f5fe4
aziz gharbi <agharbi@inovva.com>
2018-06-13 23:47:44 -0400

    testing

---
+++ functions.js
@@ -0,0 +1,12 @@
+
+const x = {
+    val: 8
+  };
+  
+  const z = x => Object.assign({}, x, { val: x.val + 1});
+  
+  const y = x => Object.assign({}, x, { val: x.val * 2});
+  
+  
+  console.log(z(y(x)))
+  console.log(y(z(x)))
