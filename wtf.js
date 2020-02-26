49f753b72fc1272a90694f4f45bd6e3b1adbbdf9
aziz gharbi <med.aziz,gharbii@gmail.com>
2020-02-25 23:45:51 -0500

    wtf js2

---
+++ wtf.js
@@ -0,0 +1,39 @@
+function sayMe() {
+  this.name = "aziz";
+  return function hello() {
+    let self = this;
+    self.age = "26";
+    //console.log(this.age);
+    return this.name;
+  };
+}
+
+function sayYou() {
+  this.name = "sam";
+  return () => {
+    let self = this;
+    self.age = "28";
+    //console.log(this.age);
+    return this.name;
+  };
+}
+
+const foo = sayMe()();
+
+const bar = sayYou()();
+
+console.log(bar);
+console.log(foo);
+
+let dog = {
+  sound: "woof",
+  talk: function() {
+    console.log(this.sound);
+  }
+};
+
+dog.talk();
+
+let talkFunction = dog.talk;
+
+talkFunction();
